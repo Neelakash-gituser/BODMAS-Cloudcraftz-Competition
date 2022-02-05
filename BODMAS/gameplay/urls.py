2ee3d63d6e444d4e5653dde173e380ec56bc1214
Neelakash-gituser <gamingdeity98@gmail.com>
2022-02-05 18:44:20 +0530

    Under development

--- BODMAS/gameplay/urls.py
+++ BODMAS/gameplay/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('game', views.solution, name='solution')
+    path('game', views.solution, name='solution'),
+    path('yourturn', views.users, name='users')
 ]
--- BODMAS/gameplay/views.py
+++ BODMAS/gameplay/views.py
@@ -1,3 +1,4 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 from .models import Solution, BODMAS
 
@@ -6,4 +7,10 @@ def home(request):
     return render(request, 'Homepage.html')
 
 def solution(request):
-    pass
+    obj = Solution()
+    N1, O1, N2, O2, N3, N4 = obj.new_problem()
+    context = {'N1': N1, 'O1': O1, 'N2': N2, 'O2': O2, 'N3': N3, 'N4': N4}
+    return render(request, 'Gameplay.html', {'context': context})
+
+def users(request):
+    return render(request, 'Result.html')
