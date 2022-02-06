d7bd7ffc24133b72e2040d385f800fa70b9be9ae
Neelakash-gituser <gamingdeity98@gmail.com>
2022-02-06 16:31:35 +0530

    Suggestions Implemented

--- BODMAS.py
+++ BODMAS.py
@@ -3,6 +3,22 @@ import numpy as np
 # This is the class which generates the random equation along with the random output i.e. N4 .
 class Solution:
 
+    def new_problem_neg(self, disallow=True):
+        # disallow negative results .
+        while disallow:
+            operators = ['-', '+', '*', '/']
+            N1, N2, N3 = np.random.randint(0, 9, 3)
+            x, y = np.random.randint(0, 3, 2)
+            O1, O2 = operators[x], operators[y]
+
+            ans = str(N1) + O1 + str(N2) + O2 + str(N3)
+            N4 = eval(ans)
+
+            if N4 >= 0:
+                disallow = False
+
+        return N1, O1, N2, O2, N3, N4
+
     def new_problem(self):
         operators = ['-', '+', '*', '/']
         N1, N2, N3 = np.random.randint(0, 9, 3)
@@ -24,7 +40,7 @@ class BODMAS:
     operands = [i for i in range(10)]
 
     # default Constructor
-    def __init__(self, N1, O1, N2, O2, N3, opt_N1, opt_O1, opt_N2, opt_O2, opt_N3, opt_N4, i, n):
+    def __init__(self, N1, O1, N2, O2, N3, opt_N1, opt_O1, opt_N2, opt_O2, opt_N3, opt_N4):
         self.N1 = N1
         self.N2 = N2
         self.N3 = N3
@@ -36,8 +52,6 @@ class BODMAS:
         self.opt_N4 = opt_N4
         self.opt_O1 = opt_O1
         self.opt_O2 = opt_O2
-        self.i = i
-        self.n = n
 
         # check whether the input is valid or not
         self.check_input()
@@ -82,8 +96,6 @@ class BODMAS:
             val = self._cal_value()
             if val != self.opt_N4:
                 print("Your RHS not equal to your LHS")
-                if self.i == self.n - 1:
-                    print(f"Original equation was -> {str(self.opt_N1) + blank + self.opt_O1 + blank + str(self.opt_N2) + blank + self.opt_O2 + blank + str(self.opt_N3)}")
             else:
                 self.check_positions()
 
@@ -98,85 +110,94 @@ class BODMAS:
 
         if self.N1 in your_eq:
             if self.N1 == self.opt_N1:
-                green.append("N1")
+                green.append(f"N1: {self.N1}")
             else:
-                yellow.append("N1")
+                yellow.append(f"N1: {self.N1}")
         else:
-            red.append("N1")
+            red.append(f"N1: {self.N1}")
 
 
         if self.N2 in your_eq:
             if self.N2 == self.opt_N2:
-                green.append("N2")
+                green.append(f"N2: {self.N2}")
             else:
-                yellow.append("N2")
+                yellow.append(f"N2: {self.N2}")
         else:
-            red.append("N2")
+            red.append(f"N2: {self.N2}")
 
 
         if self.N3 in your_eq:
             if self.N3 == self.opt_N3:
-                green.append("N3")
+                green.append(f"N3: {self.N3}")
             else:
-                yellow.append("N3")
+                yellow.append(f"N3: {self.N3}")
         else:
-            red.append("N3")
+            red.append(f"N3: {self.N3}")
 
 
         if self.O1 in your_eq:
             if self.O1 == self.opt_O1:
-                green.append("O1")
+                green.append(f"O1: {self.O1}")
             else:
-                yellow.append("O1")
+                yellow.append(f"O1: {self.O1}")
         else:
-            red.append("O1")
+            red.append(f"O1: {self.O1}")
 
 
         if self.O2 in your_eq:
             if self.O2 == self.opt_O2:
-                green.append("O2")
+                green.append(f"O2: {self.O2}")
             else:
-                yellow.append("O2")
+                yellow.append(f"O2: {self.O2}")
         else:
-            red.append("O2")
+            red.append(f"O2: {self.O2}")
 
         print(f"Red - {red}\nYellow - {yellow}\nGreen - {green}")
 
         if len(green) == 5:
             print("\nCongratulations !!! You've successfully guessed the right equation.")
             quit()
-        if self.i == self.n-1:
-            print("\nYou've exhausted all your chances, better luck next time\n")
-            print(f"Original equation was -> {str(self.opt_N1) + blank + self.opt_O1 + blank + str(self.opt_N2) + blank + self.opt_O2 + blank + str(self.opt_N3)}")
-
 
 
 
 # The main body or the driver code:
-obj = Solution()
-A, B, C, D, E, F = obj.new_problem()
+while True:
+    print("\nNote: If you don't want the final answer that is R.H.S to be a negative number press - [Y/y]")
+    print("Disallow Negative numbers as result ? [Y - Yes]\t")
+    disallow = input('-> ').lower()
+    obj = Solution()
 
-print("The magical number is: {}\n".format(F))
+    if disallow == 'y':
+        A, B, C, D, E, F = obj.new_problem_neg()
+    else:
+        A, B, C, D, E, F = obj.new_problem()
 
-# Number of guesses permitted
-try:
-    n = int(input('Enter the number of chances: '))
-except:
-    print("A numerical value is expected")
-    quit()
 
-# loops until the players have exhausted all their chances or they've guessed it correctly whichever comes first.
-for i in range(n):
-    if i == n-1:
-        print("This is your final try !!!\n")
+    print("The magical number is: {}\n".format(F))
+    print("Input Format:\n1. Enter N1, N2, N3 each of them must be a single digit positive integer between 0 and 9.\n2. O1 and O2 are operators -> '+', '-', '*' or '/'\n")
 
-    try:
-        N1, N2, N3 = map(int, input('Enter N1, N2, N3 in the given order: ').split())
-    except:
-        print("A numerical value is expected")
-        continue
 
-    O1, O2 = input('Enter O1 and O2 respectively: ').split()
+    # loops until the players have exhausted all their chances or they've guessed it correctly whichever comes first.
+    while True:
+        blank = " "
+        try:
+            N1, N2, N3 = map(int, input('Enter N1, N2, N3 in the given order: ').split())
+        except:
+            print("A numerical value is expected")
+            continue
 
-    obj_new = BODMAS(N1, O1, N2, O2, N3, A, B, C, D, E, F, i, n)
-    print("\n")
+        O1, O2 = input('Enter O1 and O2 respectively: ').split()
+
+        obj_new = BODMAS(N1, O1, N2, O2, N3, A, B, C, D, E, F)
+
+        print("\nWant to try Again? 1 - Yes , 0 - No")
+        ans = int(input('-> '))
+        if ans != 1:
+            print(f"\nOriginal equation was -> {str(A) + blank + B + blank + str(C) + blank + D + blank + str(E) + ' = ' + str(F)}")
+            print("\n======================================================================================================================================")
+            break
+        
+    print("\nStart new game ? [Y/N]")
+    new = input('-> ').lower()
+    if new != 'y':
+        break
